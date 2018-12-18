83a825891914912f4babb53e159b3581cb6a9f08
Rory Magee <rory.magee2@gmail.com>
2018-12-18 19:51:31 +0000

    added routes for each category

--- server/Routes/main.js
+++ server/Routes/main.js
@@ -6,7 +6,62 @@ const newsapi = new newsAPI(process.env.NewsApiKey);
 
 router.get('/', (req, res, next) => {
     newsapi.v2.topHeadlines({
-        sources: 'bbc-news',
+        country: 'gb'
+    }).then(response => {
+        res.json({
+            response: response
+        });
+    });
+});
+
+router.get('/business', (req, res, next) => {
+    newsapi.v2.topHeadlines({
+        category: 'business',
+        country: 'gb'
+    }).then(response => {
+        res.json({
+            response: response
+        });
+    });
+});
+
+router.get('/entertainment', (req, res, next) => {
+    newsapi.v2.topHeadlines({
+        category: 'entertainment',
+        country: 'gb'
+    }).then(response => {
+        res.json({
+            response: response
+        });
+    });
+});
+
+router.get('/sport', (req, res, next) => {
+    newsapi.v2.topHeadlines({
+        category: 'sport',
+        country: 'gb'
+    }).then(response => {
+        res.json({
+            response: response
+        });
+    });
+});
+
+router.get('/politics', (req, res, next) => {
+    newsapi.v2.topHeadlines({
+        category: 'politics',
+        country: 'gb'
+    }).then(response => {
+        res.json({
+            response: response
+        });
+    });
+});
+
+router.get('/science', (req, res, next) => {
+    newsapi.v2.topHeadlines({
+        category: 'science',
+        country: 'gb'
     }).then(response => {
         res.json({
             response: response
