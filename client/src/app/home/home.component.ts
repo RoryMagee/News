719ce8868928b77f2618930748bfc3f2ac7723d1
Rory Magee <rory.magee2@gmail.com>
2018-12-19 21:26:52 +0000

    Altered homepage design

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -8,17 +8,19 @@ import { environment } from '../../environments/environment'
 })
 export class HomeComponent implements OnInit {
   articles: any[] = [];
-  carousel: any[] = [];
+  //carousel: any[] = [];
+  mainArticle: any;
   constructor(private rest: RestApiService) { }
 
   async ngOnInit() {
     try {
-      const data = await this.rest.get(environment.url + '/api/');
+      const data = await this.rest.get(environment.url + '/api/science');
       data['response']['status'] ? this.articles = data['response']['articles'] : console.log("Data not OK");
-      for(var x = 0; x < 5; x++) {
-        this.carousel.push(this.articles.pop());
-      }
-      console.log(this.carousel);
+      this.mainArticle = this.articles.pop();
+      // for(var x = 0; x < 5; x++) {
+      //   this.carousel.push(this.articles.pop());
+      // }
+      //console.log(this.carousel);
       console.log(this.articles);
     } catch(error) {
       console.log("Error connecting to server");
