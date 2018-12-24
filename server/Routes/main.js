c89976180fa1e6ebd7c1a0f9ba7c5a07652a1251
Rory Magee <rory.magee2@gmail.com>
2018-12-24 17:14:01 +0000

    updated categories and fixed article validation

--- server/Routes/main.js
+++ server/Routes/main.js
@@ -13,8 +13,7 @@ router.get('/', (req, res, next) => {
             var content = JSON.stringify(response.articles[x].content);
             content = content.replace(content.substring(0,1), '');
             for(y = content.length; y > 0; y--) {
-                if(content.charAt(y)=== '[') {
-                    //var ret = content.replace(content.substring(y, content.length), '');
+                if(content.charAt(y) === '[') {
                     response.articles[x].content = content.replace(content.substring(y, content.length), '');
                 }
             }
@@ -34,16 +33,10 @@ router.get('/:category', (req, res, next) => {
         for(x = 0; x < articleAmnt; x++) {
             var content = JSON.stringify(response.articles[x].content);
             for(y = content.length; y > 0; y--) {
-                if(content.charAt(y)=== '[') {
+                if(content.charAt(y) === '[') {
                     var ret = content.replace(content.substring(y, content.length), '');
                     response.articles[x].content = ret;
                 }
-            }
-        }
-        for(let x = 0; x < 20; x++) {
-            if(response.articles[x].source.id === 'null') {
-                console.log("null source");
-                delete response.articles[x];
             }
         }
         res.json({
@@ -52,88 +45,5 @@ router.get('/:category', (req, res, next) => {
     });
 });
 
-// router.get('/entertainment', (req, res, next) => {
-//     newsapi.v2.topHeadlines({
-//         category: 'entertainment',
-//         country: 'gb'
-//     }).then(response => {
-//         let articleAmnt = Object.keys(response.articles).length;
-//         for(x = 0; x < articleAmnt; x++) {
-//             var content = JSON.stringify(response.articles[x].content);
-//             for(y = content.length; y > 0; y--) {
-//                 if(content.charAt(y)=== '[') {
-//                     var ret = content.replace(content.substring(y, content.length), '');
-//                     response.articles[x].content = ret;
-//                 }
-//             }
-//         }
-//         res.json({
-//             response: response
-//         });
-//     });
-// });
-
-// router.get('/sport', (req, res, next) => {
-//     newsapi.v2.topHeadlines({
-//         category: 'sport',
-//         country: 'gb'
-//     }).then(response => {
-//         let articleAmnt = Object.keys(response.articles).length;
-//         for(x = 0; x < articleAmnt; x++) {
-//             var content = JSON.stringify(response.articles[x].content);
-//             for(y = content.length; y > 0; y--) {
-//                 if(content.charAt(y)=== '[') {
-//                     var ret = content.replace(content.substring(y, content.length), '');
-//                     response.articles[x].content = ret;
-//                 }
-//             }
-//         }
-//         res.json({
-//             response: response
-//         });
-//     });
-// });
-
-// router.get('/politics', (req, res, next) => {
-//     newsapi.v2.topHeadlines({
-//         category: 'politics',
-//         country: 'gb'
-//     }).then(response => {
-//         let articleAmnt = Object.keys(response.articles).length;
-//         for(x = 0; x < articleAmnt; x++) {
-//             var content = JSON.stringify(response.articles[x].content);
-//             for(y = content.length; y > 0; y--) {
-//                 if(content.charAt(y)=== '[') {
-//                     var ret = content.replace(content.substring(y, content.length), '');
-//                     response.articles[x].content = ret;
-//                 }
-//             }
-//         }
-//         res.json({
-//             response: response
-//         });
-//     });
-// });
-
-// router.get('/science', (req, res, next) => {
-//     newsapi.v2.topHeadlines({
-//         category: 'science',
-//         country: 'gb'
-//     }).then(response => {
-//         let articleAmnt = Object.keys(response.articles).length;
-//         for(x = 0; x < articleAmnt; x++) {
-//             var content = JSON.stringify(response.articles[x].content);
-//             for(y = content.length; y > 0; y--) {
-//                 if(content.charAt(y)=== '[') {
-//                     var ret = content.replace(content.substring(y, content.length), '');
-//                     response.articles[x].content = ret;
-//                 }
-//             }
-//         }
-//         res.json({
-//             response: response
-//         });
-//     });
-// });
 
 module.exports = router;
