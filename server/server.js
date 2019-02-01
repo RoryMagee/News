787111ed94978ab087501852f295dc767fef3e79
Rory Magee <rory.magee2@gmail.com>
2019-02-01 20:56:49 +0000

    CORS

--- server/server.js
+++ server/server.js
@@ -12,12 +12,8 @@ app.use(express.static('dist/client'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(morgan('dev'));
-//app.use(cors({origin: 'null'}));
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+app.use(cors());
+
   
 
 app.set('port', config.port);
